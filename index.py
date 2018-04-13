fd360abb90809ee82e5bbea532b435581d2861f2
m197104 <38133555+m197104@users.noreply.github.com>
2018-04-13 09:37:59 -0400

    L9:37

--- index.py
+++ index.py
@@ -3,88 +3,119 @@
 import cgi,cgitb
 #connects to mysql
 
-cgitb.enable()
-
 import mysql.connector
 from mysql.connector import errorcode
-
+from signup import users
 import config
-
-try:
-  cnx = mysql.connector.connect(user=config.USER,
-                                password = config.PASSWORD,
-                                host = config.HOST,
-                                database=config.DATABASE)
-
-except mysql.connector.Error as err:
-  print ( "Content-type: text/html" )
-  print()
-  print ("""\
-  <!DOCTYPE html>
-  <html>
-  <head>
-  <meta charset = "utf-8">
-  <title>DB connection with Python</title>
-  <style type = "text/css">
-  table, td, th {border:1px solid black}
-  </style>
-  </head>
-  <body>
-  """)
-  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    print("Something is wrong with your user name or password")
-  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    print("Database does not exist")
-  else:
-    print(err)
-  print("<p>Fix your code or Contact the system admin</p></body></html>")
-  quit()
-
-cursor = cnx.cursor()
+cgitb.enable()
 params = cgi.FieldStorage()
 
+def main():
+    try:
+        cnx = mysql.connector.connect(user=config.USER,
+                                        password = config.PASSWORD,
+                                        host = config.HOST,
+                                        database=config.DATABASE)
+
+    except mysql.connector.Error as err:
+        print ( "Content-type: text/html" )
+        print()
+        print ("""\
+          <!DOCTYPE html>
+          <html>
+          <head>
+          <meta charset = "utf-8">
+          <title>DB connection with Python</title>
+          <style type = "text/css">
+          table, td, th {border:1px solid black}
+          </style>
+          </head>
+          <body>
+          """)
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+            print("<p>Fix your code or Contact the system admin</p></body></html>")
+        quit()
+    cursor = cnx.cursor()
+    if len(params) == 3:
+        #return
+        returnval = signup(cursor)
+    else:
+        #return
+        returnval = logon(cursor)
+    cursor.close()   #commit the transaction
+    cnx.commit()  #this is really important otherwise all changes lost
+    #close connection
+    cnx.close()
+    return returnval
+    #quit()
+
+
 #code for inserting a user
-insertButton = params.getvalue("Sign Up")
+#insertButton = params.getvalue("Sign Up")
 
 #if insert button was pushed
-if insertButton:
-  #get the artist and title from the form
-  user = params.getvalue("user")
-  password = params.getvalue("pass")
+def signup(cursor):
+    #get the artist and du to inform them of the time this error occurred, and the actions you performed just before this error.More information about this error may be available in the server errortitle from the form
+    user = params.getvalue("user")
+    password = params.getvalue("pass")
+    name = params.getvalue("name")
+    #call the add song function in song to insert the song
+    result = users.addUser(cursor, user, name, password)
+    #print either a confirmation message or error message
+    if result:
+        print ('<h2>New user with id ' + str(result) + ' inserted into the database</h2>')
+    else:
+        print ('<h2>Could not insert the user</h2>')
+    #now need to clean up database cursor, etc
 
-  #call the add song function in song to insert the song
-  result = user.addUser(cursor, user, password)
-  #print either a confirmation message or error message
-  if result:
-    print ('<h2>New user with id ' + str(result) + ' inserted into the database</h2>')
-  else:
-   print ('<h2>Could not insert the user</h2>')
-  
-  
-  
-loginButton = params.getvalue("Login")
+    return
 
-#if insert button was pushed
-if loginButton:
-  #get the artist and title from the form
-  user = params.getvalue("user")
-  password = params.getvalue("pass")
-  #call authenticaTeUser
-  legit = user.authenticateUser(cursor, user, password)
-  if (legit):    #user is in database
-    #generate a random session ID cookie that has a set expiration date 
-    return HttpResponseRedirect('/messageBoard.html')        #redirect to messageBoard.html
-  
-  else:
+
+
+#code to log on a USER
+def logon(cursor):
+    #cursor = connect()
+    #get the artist and title from the form
+    username = params.getvalue("user")
+    password = params.getvalue("pass")
+    #call authenticaTeUser
+    legit = users.authenticateUser(users, cursor, username, password)
+
+    if (legit):    #user is in database
+        #generate a random session ID cookie that has a set expiration date
+        print ( "Content-type: text/html" )
+        print( "Status: 303 See Other" )
+        print( "Location: messageBoard.py" )
+        print()
+        print ("""\
+          <!DOCTYPE html>
+          <html>
+          <head>
+          <meta charset = "utf-8">
+          <title>DB connection with Python</title>
+          <style type = "text/css">
+          table, td, th {border:1px solid black}
+          </style>
+          </head>
+          <body>
+          """)
+
+        #print('<h3>redirect</h3>')
+        #return HttpResponseRedirect('messageBoard.html')        #redirect to messageBoard.html
+        print("</body></html>")
+    else:
     #user is not in database
-    print('<h2>Invalid login</h2'>)
-    
-    
-    
-#now need to clean up database cursor, etc
-cursor.close()
-#commit the transaction
-cnx.commit()  #this is really important otherwise all changes lost
-#close connection
-cnx.close()
-quit()
+        print('<h2>Invalid login</h2>')
+        quit()
+
+    #now need to clean up database cursor, etc
+
+
+
+if __name__ == "__main__":
+    main()
--- signup.py
+++ signup.py
@@ -1,77 +1,63 @@
-
+import hashlib
 # user class- adds, deletes, prints users
 
-class user:
-  # This class is used to create and maintain user accounts
+class users:
+      # This class is used to create and maintain user accounts
 
-  def __init__(self):
-    pass
+      def __init__(self):
+        pass
 
-  #add a user to the database
-  #songID is AUTO_INCREMENT and votes has a default of 0, so no need to worry about them
-  def addUser(cursor, user, password):
-    #create query statement
-    query = "Insert into Users(Username, Password) values (%s, PASSWORD(%s) )"   # + user + "','" + password + "')"
-    #execute the query
-    try:
-      cursor.execute(query, user, password) 
-      print ("<p> Executed statement: " + cursor.statement + "</p>")
-    except mysql.connector.Error as err:
-      #for DEBUG only we'll print the error - we should print some generic message instead for production site
-      print ('<p style = "color:red">')
-      print (err)
-      print ('</p>')
+      #add a user to the database
+      #songID is AUTO_INCREMENT and votes has a default of 0, so no need to worry about them
+      def addUser(self, cursor, user, name, password):
+        #Hash the Password
+        hash_object = hashlib.sha512(bytes(password, "utf-8"))
+        hashPass = hash_object.hexdigest()
+        #create query statement
+        query = "INSERT INTO Users VALUES (%s, %s, %s);"   # + user + "','" + password + "')"
+        #execute the query
+        try:
+          cursor.execute(query, user, name, hashPass)
+          print ("<p> Executed statement: " + cursor.statement + "</p>")
+        except mysql.connector.Error as err:
+          #for DEBUG only we'll print the error - we should print some generic message instead for production site
+          print ('<p style = "color:red">')
+          print (err)
+          print ('</p>')
 
-    #check number of rows affected > 0 if insert successful
-    nbRowsInserted = cursor.rowcount
-    userID = cursor.lastrowid #get the last songid inserted
+          #INSERT INTO Users(Username, Password) VALUES (m197104, PASSWORD('1234') );
 
-    if nbRowsInserted > 0:
+        #check number of rows affected > 0 if insert successful
+        nbRowsInserted = cursor.rowcount
+        userID = cursor.lastrowid #get the last songid inserted
 
-      return Username
-    else:
-      return False
+        if nbRowsInserted > 0:
 
-def authenticateUser(cursor, user, pass): 
-  #if user and pass is correct and in database return True
-  query = "SELECT Password FROM Users WHERE Username = %s'    #gives hashed password in database
-  cursor.execute(query, user)
-  hashedpass = cursor.lastrow
-  if (hashedpass == Null):
-    return False
-  query2 = "SELECT PASSWORD(%s); "
-  cursor.execeute(query2, pass)
-  givenpass = cursor.lastrow
-  if (givenpass == hashedpass):
-    return True
-  else:
-    return False
-    
-"""
-  def printUsers(cursor):
-    query = "SELECT userID, user, pass FROM Users ORDER BY user"
-    # query = "SELECT Artist FROM songs ORDER BY Artist, Title"  # if only one column returned, make sure we read it as a tuple of 1 element, which is (col1,)
-    try:
-      cursor.execute(query)
-    except mysql.connector.Error as err:
-      #for DEBUG only we'll print the error and statement- we should print some generic message instead for production site
-      print ('<p style = "color:red">')
-      print(err)
-      print (" for statement" + cursor.statement )
-      print ('</p>')
+          return user
+        else:
+          return False
 
-    nbRows = 0
-    #create a table with results
-    
-  
-    table = "<table><tr><th>userID</th><th>Artist</th><th>Title</th><th>Votes</th></tr>\n"
-    for (songID, artist, title, votes) in cursor:
-    # for (artist,) in cursor: #do something like this if only one column, artist in the example, is returned - needs to be a tuple, so have the ,
-       table += "<tr><td>"+str(songID) + "</td><td>" + artist+"</td><td>"+title+"</td><td>" + str(votes) + "</td></tr>\n"
-       nbRows+=1
-    table += "</table>"
+      def authenticateUser(self, cursor, username, password):
+          #query = "SELECT Password FROM Users WHERE Username = '%s';"    #gives hashed password in database
+          query = "SELECT Password FROM Users WHERE Username = 'm197102';"    #gives hashed password in database
+          cursor.execute(query, username)
+          hashedpassJK = cursor.fetchone()
+          hashedpass = str(hashedpassJK[0])
+          #print(hashedpass)
+          #print(type(hashedpass))
 
-    if nbRows > 0:
-      return table
-    else:
-      return ""
+          if (hashedpass):
+              #print("hello laurel " + str(password))
+              #query2 = "SELECT PASSWORD(%s) "
+              hash_object = hashlib.sha512(bytes(password, "utf-8"))
+              givenpass = hash_object.hexdigest()
+              #cursor.execute(query2, password)
+              #givenpass = cursor.fetchone()
+              #print(givenpass)
+              #print(type(givenpass))
+              if (givenpass == hashedpass):
+                  return True
+              else:
+                  return False
+          else:
+              return False
